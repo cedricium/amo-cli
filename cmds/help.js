1625df95796b1c18215311cf398356490888b967
Cedricium <amaya.cedric@gmail.com>
2018-05-04 11:22:35 -0700

    Update `help` command to display current arguments + fix output

--- cmds/help.js
+++ cmds/help.js
@@ -5,11 +5,12 @@ const commands = 'Commands:'.blue;
 const flags = 'Flags:'.blue;
 const flagsOptional = '[flags]'.blue;
 const note = 'Note:'.bold;
+const amoLink = 'https://addons.mozilla.org'.blue;
 
 const menus = {
-  main: `${usage} amo COMMAND
+  main: `${usage} amo <command> <args>
 
-${'amo-cli is a CLI application used to interact with Firefox AMO (addons.mozilla.org).'.bold}
+${`amo-cli is a CLI tool used to interact with the Firefox AMO (${amoLink}).`.bold}
 
 ${commands}
  ${'featured'.padEnd(16)} ${'list featured add-ons'.dim}
@@ -22,6 +23,7 @@ ${commands}
 ${'list featured add-ons matching some parameters'.bold}
 
 ${flags}
+ ${'--language, -l'.padEnd(16)} ${'add-ons featured for this specific language'.dim}
  ${'--page-size, -p'.padEnd(16)} ${'the number of add-ons to get'.dim}
  ${'--show-url, -u'.padEnd(16)} ${`display the add-on's AMO url`.dim}
  ${'--type, -t'.padEnd(16)} ${'filter by add-on type'.dim}\n`,
@@ -31,10 +33,11 @@ ${flags}
 ${'search through public add-ons with the ability to filter results'.bold}
 
 ${flags}
-  ${'--page-size, -p'.padEnd(16)} ${'the number of add-ons to get'.dim}
-  ${'--language, -l'.padEnd(16)} ${'add-ons featured for this specific language'.dim}
-  ${'--show-url, -u'.padEnd(16)} ${`display the add-on's AMO url`.dim}
-  ${'--type, -t'.padEnd(16)} ${'filter by add-on type'.dim}\n`,
+ ${'--query, -q'.padEnd(16)} ${'the search query (maximum allowed length is 100 characters'.dim}
+ ${'--page-size, -p'.padEnd(16)} ${'the number of add-ons to get'.dim}
+ ${'--show-url, -u'.padEnd(16)} ${`display the add-on's AMO url`.dim}
+ ${'--sort, -s'.padEnd(16)} ${'sorting parameters'.dim}
+ ${'--type, -t'.padEnd(16)} ${'filter by add-on type'.dim}\n`,
 };
 
 module.exports = (args) => {
